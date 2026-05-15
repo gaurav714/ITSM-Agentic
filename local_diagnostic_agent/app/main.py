1df19bebdc0319da83cad733a0da387f6c805ccc
gaurav <gaurav.nauyital@everi.com>
2026-05-15 12:28:40 +0530

    Agent Build

--- local_diagnostic_agent/app/main.py
+++ local_diagnostic_agent/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://192.168.29.13:5173",
     ],
     allow_credentials=False,
     allow_methods=["POST", "GET", "OPTIONS"],
